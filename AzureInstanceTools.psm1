2c99669d3ab07a746bcca301d78a3a1247632685
Laureano Remedi <lremedi@gmail.com>
2014-09-30 17:49:27 -0300

    Dry run (Make it run)

---
+++ AzureInstanceTools.psm1
@@ -0,0 +1,34 @@
+function Stop-AllAzureInstances {
+param(
+    $exclude
+    )
+    $instances = $null
+    if($exclude -eq $null) {
+       $instances = Get-AzureVM
+    } else {
+        $instances = Get-AzureInstancesWithName "*" -exclude $exclude
+    }
+    $instances | `
+    % { 
+        Write-Host "Stopping $($_.name)" 
+        Write-Host "Stop-AzureVM -StayProvisioned -Force"
+    }
+}
+
+function Get-AzureInstancesWithName {
+param(
+[parameter(Mandatory=$true,Position=0)]$name,
+[parameter(Mandatory=$false,Position=1)]$exclude)
+    $instances = Get-AzureVM | ? { $_.Name -Like $name}
+    if($exclude -ne $null) {
+        $instances = $instances | ? {
+            $name = $_.Name
+            $shouldPass = $true
+            $exclude | % { $shouldPass = ($shouldPass -and $name -notlike $_)}
+            $shouldPass
+        }
+    }
+    $instances
+}
+
+Export-ModuleMember -Function Stop-AllAzureInstances, Get-AzureInstancesWithName
